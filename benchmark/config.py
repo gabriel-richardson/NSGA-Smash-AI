aa90ca302efe5016fb78afe5eaf3302b2606744a
Gabriel Richardson <gabriel_richardson_97@hotmail.com>
2017-11-04 14:05:03 -0500

    add benchmark

---
+++ benchmark/config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# configuration for game
+game = dict(
+    stage_width         = 85.5656967163086, # HALF the stage width of FINAL DESTINATION
+    g_time              = 4000, # game time
+    t_time              = 4000, # game testing time
+    fps                 = 60, # frame per second
+    delay               = 20, # terminal update delay
+    n_best              = 5, # number of best agents
+    n_agents            = 30, # number of agents
+)
+
+nnet = dict(
+    n_inputs    = 28,
+    n_h_neurons = 40,
+    n_outputs   = 14,
+    n_weights   = 30, # (inputs + 1) * (h_neurons) + (h_neurons + 1) * (outputs)
+)
